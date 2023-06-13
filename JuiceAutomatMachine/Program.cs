03551a7dbfcdbb878aef8cb1f52ad2f269209448
Oleg0392 <genri.kocsfa@gmail.com>
2023-06-14 02:10:15 +0500

    Initial commit

---
+++ JuiceAutomatMachine/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+//dbContexts here
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+/*builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();*/
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    /*app.UseSwagger();
+    app.UseSwaggerUI();*/
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+app.UseStaticFiles();
+app.UseCors("AllowAll");
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
